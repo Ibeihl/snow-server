1e6e7bd6d41d31a7454bdd4aff7e527c17d9418c
Ian Beihl <ianbeihl@gmail.com>
2018-08-16 17:50:01 -0700

    added dropdown displayCat functionality

--- routes/drinks.js
+++ routes/drinks.js
@@ -11,35 +11,41 @@ const passport = require('passport');
 const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
 
 router.get('/', jwtAuth, (req, res, next) => {
-    const { search, user } = req.query;
-    // console.log(req.query);
-
+    const { search, user, displayCat } = req.query;
+    let filter;
+    let userFilter;
     if (search) {
         const re = new RegExp(search, 'i');
-        let filter = {
-            $and: [
-                { $or: [{ 'user': user }, { 'user': 'classic' }] },
-                {
-                    $or: [{ 'name': re }, { 'ingredients': re },
-                    { 'glass': re }, { 'instructions': re }]
-                }
-            ]
+        filter = {
+            $or: [{ 'name': re }, { 'ingredients': re },
+            { 'glass': re }, { 'instructions': re }]
         }
-
-        Drink.find(filter)
-            .sort({ 'name': 'asc' })
-            .then(drinks => {
-                res.json(drinks);
-            })
-            .catch(err => next(err));
-    } else {
-        Drink.find()
+    }
+    if (displayCat === 'userDrinks') {
+        userFilter = user;
+    }
+    if (displayCat === 'classic') {
+        userFilter = 'classic';
+    }
+    if (displayCat === 'all') {
+        userFilter = [user, 'classic'];
+    }
+    if (displayCat === 'favorites') {
+        Drink.find({ 'favorites': user })
             .sort({ 'name': 'asc' })
             .then(drinks => {
                 res.json(drinks);
             })
             .catch(err => next(err));
     }
+    Drink.find(filter)
+        .where('user').in(userFilter)
+        .sort({ 'name': 'asc' })
+        .then(drinks => {
+            res.json(drinks);
+        })
+        .catch(err => next(err));
+
 })
 
 //ADD a new drink
@@ -60,36 +66,37 @@ router.post('/', jwtAuth, (req, res, next) => {
 router.put('/:id', jwtAuth, (req, res, next) => {
     const id = req.params.id
     const { user, favorite } = req.body;
+    console.log(req.body);
     /***** Never trust users - validate input *****/
-     if (!mongoose.Types.ObjectId.isValid(id)) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
         const err = new Error('The `id` is not valid');
         err.status = 400;
         return next(err);
     }
-    if (favorite){
+    if (favorite) {
         let removeFav = {
-             $pull: {favorites: { $in: user}}
+            $pull: { favorites: { $in: user } }
         }
         Drink.findByIdAndUpdate(id, removeFav, { new: true })
-        .then(result => {
-          if (result) {
-            res.json(result);
-          } else {
-            next();
-          }
-        })
+            .then(result => {
+                if (result) {
+                    res.json(result);
+                } else {
+                    next();
+                }
+            })
     } else {
         let addFav = {
             $push: { favorites: user }
         };
         Drink.findByIdAndUpdate(id, addFav, { new: true })
-        .then(result => {
-          if (result) {
-            res.json(result);
-          } else {
-            next();
-          }
-        })
+            .then(result => {
+                if (result) {
+                    res.json(result);
+                } else {
+                    next();
+                }
+            })
     }
 
 })
