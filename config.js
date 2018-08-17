6afda644bd1f492ed1aca96b56d0001acd0ed04a
Ian Beihl <ianbeihl@gmail.com>
2018-08-16 18:13:42 -0700

    config commits

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/cocktails',
   DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://localhost/cocktails',
   TEST_DATABASE_URL:
@@ -12,9 +13,4 @@ module.exports = {
         'mongodb://localhost/test-cocktails',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
 };
