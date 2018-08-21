a39166339e3ca0e436c605559bdc04ba6107966c
Ian Beihl <ianbeihl@gmail.com>
2018-08-20 20:43:18 -0700

    less cors

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ app.use(
 );
 
 
-app.use(function(req, res, next) {
-  res.header(“Access-Control-Allow-Origin”, “*”);
-  res.header(“Access-Control-Allow-Headers”, “Origin, X-Requested-With, Content-Type, Accept”);
-  next();
- });
+// app.use(function(req, res, next) {
+//   res.header(“Access-Control-Allow-Origin”, “*”);
+//   res.header(“Access-Control-Allow-Headers”, “Origin, X-Requested-With, Content-Type, Accept”);
+//   next();
+//  });
 
 //auth strategies
 passport.use(localStrategy);
